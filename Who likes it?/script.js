286c544b8edf08e9af624fbfc7a1ee2bd7ae8adf
Khanaru0202 <74447462+Khanaru220@users.noreply.github.com>
2021-07-07 11:08:28 +0700

    Give comments about the path to solve
    Create 'Possible cases'
    Create 'Sub-problems'

--- Who likes it?/script.js
+++ Who likes it?/script.js
@@ -1,3 +1,13 @@
 function likes(names) {
-  // TODO
+  // Possible cases
+  // 1. check [empty] - [no one likes this]
+  // 2. check [1 like] - [... likes this]
+  // 3. check [2 like] - [... and ... likes this]
+  // 4. check [3 like] - [..., ... and ... likes this]
+  // 5. check [more than 3] - [..., ... and 99 others like this]
+  
+  // Sub-problems
+  // 1. counting number of array
+  // 2. check condition with number items
+  // 3. display text
 }

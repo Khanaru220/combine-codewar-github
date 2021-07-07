0ccdd2f357ff6646cae7270bcc86a7ede5d53986
Khanaru0202 <74447462+Khanaru220@users.noreply.github.com>
2021-07-07 14:22:47 +0700

    Update Who likes it?/script-switch.js
    OK i got this

--- Who likes it?/script-switch.js
+++ Who likes it?/script-switch.js
@@ -35,9 +35,7 @@ function likes(names) {
     case 3:
       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
     default:
-      return `${names[0]}, ${names[1]} and ${
-        names.length - 2
-      } others like this`;
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
   }
 }
 // Test-display

7f454090a87496ba19c14ecde32a5a0ad3d433e9
Khanaru0202 <74447462+Khanaru220@users.noreply.github.com>
2021-07-07 12:04:07 +0700

    Add solution by if statement
    Create 'use strict' mode
    Create if statement to check 5 conditions

--- Who likes it?/script.js
+++ Who likes it?/script.js
@@ -1,14 +1,38 @@
-function likes(names) {
-  // Possible cases
-  // 1. check [empty] - [no one likes this]
+'use strict';
+
+function likes(nameArray) {
+  // Posible cases
+  // 1. Check [empty] - [no one likes this]
   // 2. check [1 like] - [... likes this]
   // 3. check [2 like] - [... and ... likes this]
   // 4. check [3 like] - [..., ... and ... likes this]
   // 5. check [more than 3] - [..., ... and 99 others like this]
-  
-  // Sub-problems
-  // 1. Pass array into parameter
-  // 2. counting number of array
+
+  // Sub-problem
+  // 1. pass array into parameter
+  // 2. counting number items in array
   // 3. check condition with number items
   // 4. display text
+
+  if (nameArray.length === 0) {
+    console.log(`no one likes this`);
+  } else if (nameArray.length === 1) {
+    console.log(`${nameArray[0]} likes this`);
+  } else if (nameArray.length === 2) {
+    console.log(`${nameArray[0]} and ${nameArray[1]} likes this`);
+  } else if (nameArray.length === 3) {
+    console.log(
+      `${nameArray[0]}, ${nameArray[1]} and ${nameArray[2]} likes this`
+    );
+  } else {
+    console.log(
+      // count the left items to display 'number others'
+      `${nameArray[0]}, ${nameArray[1]} and ${
+        nameArray.length - 2
+      } others likes this`
+    );
+  }
 }
+// Test-display
+likes([]);
+likes(['Michael']);
